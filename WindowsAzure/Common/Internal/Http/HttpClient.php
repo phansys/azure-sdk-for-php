8539d361fcbea23299fbc67cbd0c4b46fa8af665
Javier Spagnoletti <phansys@gmail.com>
2016-01-24 13:00:31 -0300

    Rempved calls to `require_once()` for PEAR classes.

--- WindowsAzure/Common/Internal/Http/HttpClient.php
+++ WindowsAzure/Common/Internal/Http/HttpClient.php
@@ -13,7 +13,7 @@
  * limitations under the License.
  *
  * PHP version 5
- * 
+ *
  * @category  Microsoft
  * @package   WindowsAzure\Common\Internal\Http
  * @author    Azure PHP SDK <azurephpsdk@microsoft.com>
@@ -21,7 +21,7 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
  * @link      https://github.com/windowsazure/azure-sdk-for-php
  */
- 
+
 namespace WindowsAzure\Common\Internal\Http;
 use WindowsAzure\Common\Internal\Http\IHttpClient;
 use WindowsAzure\Common\Internal\IServiceFilter;
@@ -29,8 +29,6 @@ use WindowsAzure\Common\Internal\Resources;
 use WindowsAzure\Common\ServiceException;
 use WindowsAzure\Common\Internal\Validate;
 use WindowsAzure\Common\Internal\Http\IUrl;
-
-require_once 'HTTP/Request2.php';
 
 /**
  * HTTP client which sends and receives HTTP requests and responses.
@@ -49,32 +47,32 @@ class HttpClient implements IHttpClient
      * @var \HTTP_Request2
      */
     private $_request;
-    
+
     /**
-     * @var WindowsAzure\Common\Internal\Http\IUrl 
+     * @var WindowsAzure\Common\Internal\Http\IUrl
      */
     private $_requestUrl;
-    
+
     /**
      * Holds the latest response object
-     * 
+     *
      * @var \HTTP_Request2_Response
      */
     private $_response;
-    
+
     /**
      * Holds expected status code after sending the request.
-     * 
+     *
      * @var array
      */
     private $_expectedStatusCodes;
-    
+
     /**
      * Initializes new HttpClient object.
-     * 
+     *
      * @param string $certificatePath          The certificate path.
      * @param string $certificateAuthorityPath The path of the certificate authority.
-     * 
+     *
      * @return WindowsAzure\Common\Internal\Http\HttpClient
      */
     function __construct(
@@ -105,21 +103,21 @@ class HttpClient implements IHttpClient
             "user-agent" => null,
             "expect" => ''
         ));
-        
+
         $this->_requestUrl          = null;
         $this->_response            = null;
         $this->_expectedStatusCodes = array();
     }
-    
+
     /**
      * Makes deep copy from the current object.
-     * 
+     *
      * @return WindowsAzure\Common\Internal\Http\HttpClient
      */
     public function __clone()
     {
         $this->_request = clone $this->_request;
-        
+
         if (!is_null($this->_requestUrl)) {
             $this->_requestUrl = clone $this->_requestUrl;
         }
@@ -129,7 +127,7 @@ class HttpClient implements IHttpClient
      * Sets the request url.
      *
      * @param WindowsAzure\Common\Internal\Http\IUrl $url request url.
-     * 
+     *
      * @return none.
      */
     public function setUrl($url)
@@ -142,7 +140,7 @@ class HttpClient implements IHttpClient
      * not.
      *
      * @return WindowsAzure\Common\Internal\Http\IUrl
-     */ 
+     */
     public function getUrl()
     {
         return $this->_requestUrl;
@@ -151,9 +149,9 @@ class HttpClient implements IHttpClient
     /**
      * Sets request's HTTP method. You can use \HTTP_Request2 constants like
      * Resources::HTTP_GET or strings like 'GET'.
-     * 
+     *
      * @param string $method request's HTTP method.
-     * 
+     *
      * @return none
      */
     public function setMethod($method)
@@ -185,26 +183,26 @@ class HttpClient implements IHttpClient
     /**
      * Sets a an existing request header to value or creates a new one if the $header
      * doesn't exist.
-     * 
+     *
      * @param string $header  header name.
      * @param string $value   header value.
      * @param bool   $replace whether to replace previous header with the same name
      * or append to its value (comma separated)
-     * 
+     *
      * @return none
      */
     public function setHeader($header, $value, $replace = false)
     {
         Validate::isString($value, 'value');
-        
+
         $this->_request->setHeader($header, $value, $replace);
     }
-    
+
     /**
      * Sets request headers using array
-     * 
+     *
      * @param array $headers headers key-value array
-     * 
+     *
      * @return none
      */
     public function setHeaders($headers)
@@ -216,9 +214,9 @@ class HttpClient implements IHttpClient
 
     /**
      * Sets HTTP POST parameters.
-     * 
+     *
      * @param array $postParameters The HTTP POST parameters.
-     * 
+     *
      * @return none
      */
     public function setPostParameters($postParameters)
@@ -227,15 +225,15 @@ class HttpClient implements IHttpClient
     }
 
     /**
-     * Processes the reuqest through HTTP pipeline with passed $filters, 
+     * Processes the reuqest through HTTP pipeline with passed $filters,
      * sends HTTP request to the wire and process the response in the HTTP pipeline.
-     * 
+     *
      * @param array $filters HTTP filters which will be applied to the request before
      * send and then applied to the response.
      * @param IUrl  $url     Request url.
-     * 
+     *
      * @throws WindowsAzure\Common\ServiceException
-     * 
+     *
      * @return string The response body
      */
     public function send($filters, $url = null)
@@ -244,14 +242,14 @@ class HttpClient implements IHttpClient
             $this->setUrl($url);
             $this->_request->setUrl($this->_requestUrl->getUrl());
         }
-        
+
         $contentLength = Resources::EMPTY_STRING;
         if (    strtoupper($this->getMethod()) != Resources::HTTP_GET
             && strtoupper($this->getMethod()) != Resources::HTTP_DELETE
             && strtoupper($this->getMethod()) != Resources::HTTP_HEAD
         ) {
             $contentLength = 0;
-            
+
             if (!is_null($this->getBody())) {
                 $contentLength = strlen($this->getBody());
             }
@@ -270,7 +268,7 @@ class HttpClient implements IHttpClient
                 $this, $this->_response
             );
         }
-        
+
         self::throwIfError(
             $this->_response->getStatus(),
             $this->_response->getReasonPhrase(),
@@ -280,12 +278,12 @@ class HttpClient implements IHttpClient
 
         return $this->_response->getBody();
     }
-    
+
     /**
      * Sets successful status code
-     * 
+     *
      * @param array|string $statusCodes successful status code.
-     * 
+     *
      * @return none
      */
     public function setExpectedStatusCode($statusCodes)
@@ -296,47 +294,47 @@ class HttpClient implements IHttpClient
             $this->_expectedStatusCodes = $statusCodes;
         }
     }
-    
+
     /**
      * Gets successful status code
-     * 
+     *
      * @return array
      */
     public function getSuccessfulStatusCode()
     {
         return $this->_expectedStatusCodes;
     }
-    
+
     /**
      * Sets configuration parameter.
-     * 
+     *
      * @param string $name  The configuration parameter name.
      * @param mix    $value The configuration parameter value.
-     * 
+     *
      * @return none
      */
     public function setConfig($name, $value = null)
     {
         $this->_request->setConfig($name, $value);
     }
-    
+
     /**
      * Gets value for configuration parameter.
-     * 
+     *
      * @param string $name configuration parameter name.
-     * 
+     *
      * @return string
      */
     public function getConfig($name)
     {
         return $this->_request->getConfig($name);
     }
-    
+
     /**
      * Sets the request body.
-     * 
+     *
      * @param string $body body to use.
-     * 
+     *
      * @return none
      */
     public function setBody($body)
@@ -344,39 +342,39 @@ class HttpClient implements IHttpClient
         Validate::isString($body, 'body');
         $this->_request->setBody($body);
     }
-    
+
     /**
      * Gets the request body.
-     * 
+     *
      * @return string
      */
     public function getBody()
     {
         return $this->_request->getBody();
     }
-    
+
     /**
      * Gets the response object.
-     * 
+     *
      * @return \HTTP_Request2_Response
      */
     public function getResponse()
     {
         return $this->_response;
     }
-    
+
     /**
      * Throws ServiceException if the recieved status code is not expected.
-     * 
+     *
      * @param string $actual   The received status code.
      * @param string $reason   The reason phrase.
      * @param string $message  The detailed message (if any).
      * @param array  $expected The expected status codes.
-     * 
+     *
      * @return none
-     * 
+     *
      * @static
-     * 
+     *
      * @throws ServiceException
      */
     public static function throwIfError($actual, $reason, $message, $expected)
--- WindowsAzure/Table/Internal/MimeReaderWriter.php
+++ WindowsAzure/Table/Internal/MimeReaderWriter.php
@@ -11,7 +11,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * PHP version 5
  *
  * @category  Microsoft
@@ -21,11 +21,9 @@
  * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
  * @link      https://github.com/windowsazure/azure-sdk-for-php
  */
- 
+
 namespace WindowsAzure\Table\Internal;
-require_once 'PEAR.php';
-require_once 'Mail/mimePart.php';
-require_once 'Mail/mimeDecode.php';
+
 use WindowsAzure\Common\Internal\Resources;
 use WindowsAzure\Common\Internal\Utilities;
 
@@ -44,10 +42,10 @@ class MimeReaderWriter implements IMimeReaderWriter
 {
     /**
      * Given array of MIME parts in raw string, this function converts them into MIME
-     * representation. 
-     * 
+     * representation.
+     *
      * @param array $bodyPartContents The MIME body parts.
-     * 
+     *
      * @return array Returns array with two elements 'headers' and 'body' which
      * represents the MIME message.
      */
@@ -68,32 +66,32 @@ class MimeReaderWriter implements IMimeReaderWriter
 
         // Create changeset MIME part
         $changeSet = new \Mail_mimePart();
-        
+
         for ($i = 0; $i < $count; $i++) {
             $changeSet->addSubpart($bodyPartContents[$i], $options);
         }
-        
+
         // Encode the changeset MIME part
         $changeSetEncoded = $changeSet->encode($changeSetId);
-        
+
         // Create the batch MIME part
         $batch = new \Mail_mimePart(Resources::EMPTY_STRING, $contentType1);
-        
+
         // Add changeset encoded to batch MIME part
         $batch->addSubpart($changeSetEncoded['body'], $contentType2);
-        
+
         // Encode batch MIME part
         $batchEncoded = $batch->encode($batchId);
-        
+
         return $batchEncoded;
     }
-    
+
     /**
-     * Parses given mime HTTP response body into array. Each array element 
+     * Parses given mime HTTP response body into array. Each array element
      * represents a change set result.
-     * 
+     *
      * @param string $mimeBody The raw MIME body result.
-     * 
+     *
      * @return array
      */
     public function decodeMimeMultipart($mimeBody)
@@ -104,11 +102,11 @@ class MimeReaderWriter implements IMimeReaderWriter
         $structure                = $mimeDecoder->decode($params);
         $parts                    = $structure->parts;
         $bodies                   = array();
-        
+
         foreach ($parts as $part) {
             $bodies[] = $part->body;
         }
-        
+
         return $bodies;
     }
 }
